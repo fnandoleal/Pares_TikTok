b69297f0aeb528cd6cb0dbe54c3b28916fc0acbe
fnandoleal <fernandoleal_flbnu@yahoo.com.br>
2026-06-27 22:56:19 -0300

    Correção

--- painel.js
+++ painel.js
@@ -1,332 +1,100 @@
 import {
-bancoDados
+    bancoDados
 }
 from "./configuracao-firebase.js";
 
 import {
-collection,
-onSnapshot
+    collection,
+    onSnapshot
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 let participantes = [];
-
-let quantidadeAnterior = 0;
 
 let ultimoNomeExibido = "";
 
 let ultimaAprovacao = 0;
 
 /*
-
-# NOME EM DESTAQUE
-
+========================================
+NOME EM DESTAQUE
+========================================
 */
 
 function mostrarParticipanteDestaque() {
 
-if (
-    participantes.length === 0
-) {
-    return;
-}
+    if (
+        participantes.length === 0
+    ) {
+        return;
+    }
 
-let sorteado;
+    let sorteado;
 
-do {
+    do {
 
-    sorteado =
+        sorteado =
 
-        participantes[
-            Math.floor(
-                Math.random() *
-                participantes.length
-            )
-        ];
+            participantes[
+                Math.floor(
+                    Math.random() *
+                    participantes.length
+                )
+            ];
 
-}
-while (
+    }
+    while (
 
-    participantes.length > 1 &&
+        participantes.length > 1 &&
 
-    sorteado.tiktok ===
-    ultimoNomeExibido
+        sorteado.tiktok ===
+        ultimoNomeExibido
 
-);
-
-ultimoNomeExibido =
-    sorteado.tiktok;
-
-const nome =
-
-    document.getElementById(
-        "nomeDestaque"
     );
 
-if (!nome) {
-    return;
+    ultimoNomeExibido =
+        sorteado.tiktok;
+
+    const nome =
+
+        document.getElementById(
+            "nomeDestaque"
+        );
+
+    if (!nome) {
+        return;
+    }
+
+    nome.style.opacity = "0";
+
+    setTimeout(
+        () => {
+
+            nome.innerHTML =
+                sorteado.tiktok;
+
+            nome.style.opacity =
+                "1";
+
+        },
+        500
+    );
+
 }
-
-nome.style.opacity =
-    "0";
-
-setTimeout(
-    () => {
-
-        nome.innerHTML =
-            sorteado.tiktok;
-
-        nome.style.opacity =
-            "1";
-
-    },
-    500
-);
-
-}
-
-/*
-
-# ROTAÇÃO ALEATÓRIA
-
-*/
 
 function iniciarRotacao() {
 
-mostrarParticipanteDestaque();
+    mostrarParticipanteDestaque();
 
-const tempoAleatorio =
+    const tempoAleatorio =
 
-    Math.floor(
-        Math.random() * 2001
-    ) + 8000;
+        Math.floor(
+            Math.random() * 2001
+        ) + 8000;
 
-setTimeout(
-    iniciarRotacao,
-    tempoAleatorio
-);
-
-}
-
-/*
-
-# NOVO PARTICIPANTE
-
-*/
-
-function exibirNovoParticipante(
-tiktok
-) {
-
-const painelPrincipal =
-
-    document.getElementById(
-        "painelPrincipal"
-    );
-
-const area =
-
-    document.getElementById(
-        "novoCadastro"
-    );
-
-if (
-    !painelPrincipal ||
-    !area
-) {
-    return;
-}
-
-painelPrincipal.style.display =
-    "none";
-
-area.style.display =
-    "flex";
-
-area.innerHTML =
-
-    `
-    🎉 NOVO CADASTRO
-
-    <br><br>
-
-    ${tiktok}
-    `;
-
-const tempoAleatorio =
-
-    Math.floor(
-        Math.random() * 10001
-    ) + 30000;
-
-setTimeout(
-    () => {
-
-        area.style.display =
-            "none";
-
-        painelPrincipal.style.display =
-            "flex";
-
-    },
-    tempoAleatorio
-);
-
-}
-
-function animarTotal() {
-
-const total =
-
-    document.getElementById(
-        "totalParticipantes"
-    );
-
-if (!total) {
-    return;
-}
-
-total.style.transform =
-    "scale(1.15)";
-
-setTimeout(
-    () => {
-
-        total.style.transform =
-            "scale(1)";
-
-    },
-    300
-);
-
-}
-
-onSnapshot(
-
-collection(
-    bancoDados,
-    "perfis"
-),
-
-(snapshot) => {
-
-    const listaNova = [];
-
-    snapshot.forEach(
-        (doc) => {
-
-            const perfil =
-                doc.data();
-
-            if (
-                perfil.situacao ===
-                "ativo"
-                &&
-                perfil.tiktok
-            ) {
-
-                listaNova.push(
-                    perfil
-                );
-
-            }
-
-        }
-    );
-
-    participantes =
-        listaNova;
-
-    const totalAtual =
-        listaNova.length;
-
-    const elementoTotal =
-
-        document.getElementById(
-            "totalParticipantes"
-        );
-
-    if (
-        elementoTotal
-    ) {
-
-        elementoTotal.innerHTML =
-            totalAtual;
-
-    }
-
-    let perfilRecente = null;
-
-listaNova.forEach(
-    (perfil) => {
-
-        if (
-
-            perfil.dataAprovacaoPainel &&
-
-            perfil.dataAprovacaoPainel >
-            ultimaAprovacao
-
-        ) {
-
-            ultimaAprovacao =
-                perfil.dataAprovacaoPainel;
-
-            perfilRecente =
-                perfil;
-
-        }
-
-    }
-);
-
-if (
-    perfilRecente
-) {
-
-    animarTotal();
-
-    exibirNovoParticipante(
-        perfilRecente.tiktok
-    );
-
-} {
-
-        animarTotal();
-
-        let perfilRecente = null;
-
-listaNova.forEach(
-    (perfil) => {
-
-        if (
-
-            perfil.dataAprovacaoPainel &&
-
-            perfil.dataAprovacaoPainel >
-            ultimaAprovacao
-
-        ) {
-
-            ultimaAprovacao =
-                perfil.dataAprovacaoPainel;
-
-            perfilRecente =
-                perfil;
-
-        }
-
-    }
-);
-
-if (
-    perfilRecente
-) {
-
-    animarTotal();
-
-    exibirNovoParticipante(
-        perfilRecente.tiktok
+    setTimeout(
+        iniciarRotacao,
+        tempoAleatorio
     );
 
 }
